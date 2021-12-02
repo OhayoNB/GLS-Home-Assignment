31c77ba02a318f40f2ca11839e3fcc9e0fff88ec
ohayonb <ohayonzzb@gmail.com>
2021-12-02 21:15:57 +0200

    Load jQuery with setTimeout if failed

--- player.js
+++ player.js
@@ -5,6 +5,21 @@ script.src = jQueryUrl;
 script.type = "text/javascript";
 document.getElementsByTagName("head")[0].appendChild(script);
 
+// Import attched JSONP endpoint
+const url =
+  "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867";
+
+// Append JSONP endpoint to body
+checkIfLoaded = () => {
+  if (window.jQuery) {
+    let jsonScript = document.createElement("script");
+    jsonScript.src = url;
+    document.body.appendChild(jsonScript);
+  } else {
+    window.setTimeout(checkIfLoaded, 100);
+  }
+};
+
 // Import attached CSS file
 let link = document.createElement("link");
 link.rel = "stylesheet";
@@ -12,11 +27,3 @@ link.type = "text/css";
 link.href =
   "https://guidedlearning.oracle.com/player/latest/static/css/stTip.css";
 document.head.appendChild(link);
-
-// Import attched JSONP endpoint
-const url =
-  "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867";
-
-let jsonScript = document.createElement("script");
-jsonScript.src = url;
-document.body.appendChild(sc);
