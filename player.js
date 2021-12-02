38b4b3e43a29f8ed604ae98f36662194d116dcfa
ohayonb <ohayonzzb@gmail.com>
2021-12-02 16:57:10 +0200

    Added jQuery, CSS, JSONP files into player.js

---
+++ player.js
@@ -0,0 +1,21 @@
+// Import jQuery
+let jQueryUrl = "https://code.jquery.com/jquery-3.6.0.min.js";
+let script = document.createElement("script");
+script.src = jQueryUrl;
+script.type = "text/javascript";
+document.getElementsByTagName("head")[0].appendChild(script);
+
+// Import attached CSS file
+let link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "style.css";
+document.head.appendChild(link);
+
+// Import attched JSONP endpoint
+const url =
+  "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867";
+
+let jsonScript = document.createElement("script");
+jsonScript.src = url;
+document.body.appendChild(sc);
