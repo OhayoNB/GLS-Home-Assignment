f822d79cafb99d2b4483beff395a90ed9db80c5c
ohayonb <ohayonzzb@gmail.com>
2021-12-04 11:53:57 +0200

    Load jQuery asynchronously to use it only when it's done loaded

--- player.js
+++ player.js
@@ -1,29 +1,34 @@
-// Import jQuery
-let jQueryUrl = "https://code.jquery.com/jquery-3.6.0.min.js";
-let script = document.createElement("script");
-script.src = jQueryUrl;
-script.type = "text/javascript";
-document.getElementsByTagName("head")[0].appendChild(script);
+// Load jQuery asynchronously
+async function loadJquery() {
+  let script = document.createElement("script");
+  script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
 
-// Import attched JSONP endpoint
-const url =
-  "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867";
-
-// Append JSONP endpoint to body
-checkIfLoaded = () => {
-  if (window.jQuery) {
-    let jsonScript = document.createElement("script");
-    jsonScript.src = url;
-    document.body.appendChild(jsonScript);
-  } else {
-    window.setTimeout(checkIfLoaded, 100);
+  // Ensure that jQuery loaded successfully
+  while (
+    typeof window.jQuery === undefined ||
+    typeof window.$ === undefined ||
+    typeof $.ajax !== "function"
+  ) {
+    await sleep(1000);
   }
-};
+}
+
+// Sleep function for 1 second if jQuery not yet loaded
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+await loadJquery();
 
 // Import attached CSS file
-let link = document.createElement("link");
-link.rel = "stylesheet";
-link.type = "text/css";
-link.href =
-  "https://guidedlearning.oracle.com/player/latest/static/css/stTip.css";
-document.head.appendChild(link);
+const importCSS = () => {
+  let link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href =
+    "https://guidedlearning.oracle.com/player/latest/static/css/stTip.css";
+  document.head.appendChild(link);
+};
+
+importCSS();
